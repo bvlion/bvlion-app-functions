d1c585f539f5cfee2a79912df29459809748fd32
Bvlion <bvltiggeari@ambitious-i.net>
2020-02-04 01:10:19 +0900

    blaze プランでないと動かないっぽいが一旦コミット

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
+const Slack = require('slack')
+const slack = new Slack()
 
-exports.hello = functions.https.onRequest((request, response) => {
-  admin.database().ref('/test')
+exports.test = functions.https.onRequest((request, response) => {
+  response.status(200).send(request.body.challenge)
+})
+
+exports.hello = functions.https.onRequest((req, res) => {
+  const payload = req.body
+  console.log(payload.event)
+  admin.database().ref('/function')
     .on("value", (snapshot) => {
-      response.status(200).send(snapshot.val())
+        const element = snapshot.val()
+        console.log(element)
+        slack.chat.postMessage({
+          token: element.token,
+          channel: payload.event.channel,
+          text: element.answer,
+          attachments: [{
+              callback_id: 'deploy_button',
+              text: '',
+             actions: element.actions.map(v => ({
+                  type: 'button',
+                  text: v,
+                  name: v
+              }))
+          }]
+      }).then(console.log).catch(console.error)
     }, (errorObject) => {
-      response.status(404).send({ message: 'Not Found' })
+      res.status(404).send({ message: 'Not Found' })
     })
-});
+})
